c7d9be02ce840b0de76b64448b11b1c36eab438b
arsen-karapetyan <116805233+arsen-karapetyan@users.noreply.github.com>
2023-12-26 17:09:27 +0400

    Database Initializer

---
+++ db_script.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+
+DB_NAME = 'Chess_DB' 
+DB_OWNER = 'Karapetyan' 
+DB_URI = f'postgresql:///{DB_NAME}'
+
+
+engine = create_engine(DB_URI)
+
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+    print(f'Database {DB_NAME} created.')
+else:
+    print(f'Database {DB_NAME} already exists.')
+
+
+with engine.connect() as conn:
+    conn.execute(f'ALTER DATABASE {DB_NAME} OWNER TO {DB_OWNER}')
+    print(f'Owner of database {DB_NAME} set to {DB_OWNER}.')
